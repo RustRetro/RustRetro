86648dc1bc4227b72d8ac87c16b1f7f31484e7d5
zer0x64 <dugre.philippe@hotmail.com>
2022-02-09 22:41:07 -0500

    Added memory leak detection when building in debug

--- rustretro-wasmtime-runner/src/lib.rs
+++ rustretro-wasmtime-runner/src/lib.rs
@@ -25,6 +25,19 @@ impl Drop for Runner {
     }
 }
 
+struct WasmVec {
+    pub ptr: u32,
+    pub length: u32,
+}
+
+#[cfg(debug_assertions)]
+impl Drop for WasmVec {
+    fn drop(&mut self) {
+        // Detect if a vector hasn't been freed properly
+        panic!("A WASM vec wasn't dropped!");
+    }
+}
+
 impl Runner {
     pub fn new(core: &[u8], rom: &[u8]) -> Self {
         let engine = Engine::default();
@@ -43,9 +56,11 @@ impl Runner {
             .unwrap();
 
         // Copy the rom to WASM memory
-        let rom_buffer = wasm_alloc_vec.call(&mut store, rom.len() as u32).unwrap();
+        let rom_buffer = alloc_vec_static(&mut store, &wasm_alloc_vec, rom.len() as u32);
 
-        memory.write(&mut store, rom_buffer as usize, rom).unwrap();
+        memory
+            .write(&mut store, rom_buffer.ptr as usize, rom)
+            .unwrap();
 
         // Instanciate the emulator
         let wasm_create_core = instance
@@ -53,13 +68,11 @@ impl Runner {
             .unwrap();
 
         let emulator_pointer = wasm_create_core
-            .call(&mut store, (rom_buffer as u32, rom.len() as u32))
+            .call(&mut store, (rom_buffer.ptr, rom_buffer.length))
             .unwrap();
 
         // Free the ROM buffer
-        wasm_free_vec
-            .call(&mut store, (rom_buffer, rom.len() as u32))
-            .unwrap();
+        free_vec_static(&mut store, &wasm_free_vec, rom_buffer);
 
         let wasm_get_metadata = instance
             .get_typed_func::<u32, u64, _>(&mut store, "__rustretro_plugin_get_metadata")
@@ -69,19 +82,21 @@ impl Runner {
             .call(&mut store, emulator_pointer)
             .unwrap();
 
-        let (ptr, length) = expand_return_pointer(ptr);
+        let metadata_buffer = expand_return_pointer(ptr);
 
-        let mut metadata_bytes = vec![0u8; length as usize];
+        let mut metadata_bytes = vec![0u8; metadata_buffer.length as usize];
         memory
-            .read(&mut store, ptr as usize, &mut metadata_bytes)
+            .read(
+                &mut store,
+                metadata_buffer.ptr as usize,
+                &mut metadata_bytes,
+            )
             .unwrap();
 
         let metadata: Metadata = serde_json::from_slice(&metadata_bytes).unwrap();
 
         // Free the metadata buffer
-        wasm_free_vec
-            .call(&mut store, (ptr, length as u32))
-            .unwrap();
+        free_vec_static(&mut store, &wasm_free_vec, metadata_buffer);
 
         let wasm_controller_input = instance
             .get_typed_func::<(u32, u32), (), _>(&mut store, "__rustretro_plugin_controller_input")
@@ -123,16 +138,14 @@ impl Runner {
             .call(&mut self.store, self.emulator_pointer)
             .unwrap();
 
-        let (ptr, length) = expand_return_pointer(ptr);
+        let frame_buffer = expand_return_pointer(ptr);
 
-        let mut buffer = vec![0u8; length as usize];
+        let mut buffer = vec![0u8; frame_buffer.length as usize];
         self.memory
-            .read(&mut self.store, ptr as usize, &mut buffer)
+            .read(&mut self.store, frame_buffer.ptr as usize, &mut buffer)
             .unwrap();
 
-        self.wasm_free_vec
-            .call(&mut self.store, (ptr, buffer.len() as u32))
-            .unwrap();
+        self.free_vec(frame_buffer);
 
         buffer
     }
@@ -140,9 +153,38 @@ impl Runner {
     pub fn get_metadata(&self) -> &Metadata {
         &self.metadata
     }
+
+    fn free_vec(&mut self, wasm_vec: WasmVec) {
+        free_vec_static(&mut self.store, &self.wasm_free_vec, wasm_vec)
+    }
+}
+
+fn alloc_vec_static(
+    store: &mut Store<String>,
+    wasm_alloc_vec: &TypedFunc<u32, u32>,
+    length: u32,
+) -> WasmVec {
+    let ptr = wasm_alloc_vec.call(store, length).unwrap();
+
+    WasmVec { ptr, length }
+}
+
+fn free_vec_static(
+    store: &mut Store<String>,
+    wasm_free_vec: &TypedFunc<(u32, u32), ()>,
+    wasm_vec: WasmVec,
+) {
+    wasm_free_vec
+        .call(store, (wasm_vec.ptr, wasm_vec.length))
+        .unwrap();
+
+    std::mem::forget(wasm_vec);
 }
 
 /// Returning tuples is not well supported yet, so we return a u64 and bitmask/shift to split in into two
-fn expand_return_pointer(ptr: u64) -> (u32, u32) {
-    ((ptr & 0xFFFFFFFF) as u32, (ptr >> 32) as u32)
+fn expand_return_pointer(ptr: u64) -> WasmVec {
+    WasmVec {
+        ptr: (ptr & 0xFFFFFFFF) as u32,
+        length: (ptr >> 32) as u32,
+    }
 }
